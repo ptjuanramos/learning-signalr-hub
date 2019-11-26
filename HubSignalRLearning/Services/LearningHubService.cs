7bf8ac37d026e039629548c4e2e0430d72e2c3ac
Juan Ramos <ptjuanramos@gmail.com>
2019-11-26 12:12:53 +0000

    separated repository for hub

---
+++ HubSignalRLearning/Services/LearningHubService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HubSignalRLearning.Services
+{
+    public class LearningHubService : Hub
+    {
+        public async Task SendMessage(String user, String message)
+        {
+            await Clients.All.SendAsync("SomethingAwesome", user, message);
+        }
+
+        public async Task SendDraw(string username, byte[] drawObjectAsByteArray)
+        {
+            await Clients.All.SendAsync("NewDraw", username, drawObjectAsByteArray);
+        }
+    }
+}
